95737656709d2e038d9b1f7e8b908d344e01eed6
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2022-12-19 23:23:00 +0100

    feat: implement BrotliZlib class

---
+++ src/brotliZlib/BrotliZlib.js
@@ -0,0 +1,45 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createBrotliCompress, createBrotliDecompress } from 'node:zlib';
+
+import { getFormattedPaths } from '../services/getFormattedPaths.js';
+
+export class BrotliZlib {
+    constructor(logger) {
+        this.logger = logger;
+    }
+
+    compress = async (pathToFile, destinationPath) => {
+        try {
+            const { formattedPathToFile, formattedDestinationPath } = await getFormattedPaths({
+                pathToFile,
+                destinationPath,
+                targetExtension: '.bc',
+            });
+
+            const readStream = createReadStream(formattedPathToFile);
+            const writeStream = createWriteStream(formattedDestinationPath);
+            const brotliCompressStream = createBrotliCompress();
+
+            readStream.pipe(brotliCompressStream).pipe(writeStream);
+        } catch {
+            this.logger.logOperationFailedMessage();
+        }
+    };
+
+    decompress = async (pathToFile, destinationPath) => {
+        try {
+            const { formattedPathToFile, formattedDestinationPath } = await getFormattedPaths({
+                pathToFile,
+                destinationPath,
+            });
+
+            const readStream = createReadStream(formattedPathToFile);
+            const writeStream = createWriteStream(formattedDestinationPath);
+            const brotliDecompressStream = createBrotliDecompress();
+
+            readStream.pipe(brotliDecompressStream).pipe(writeStream);
+        } catch {
+            this.logger.logOperationFailedMessage();
+        }
+    };
+}
